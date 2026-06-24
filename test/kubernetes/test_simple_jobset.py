17a698cb177dbdadcd2845fc595795634a5566a7
Jacek J. Lakis <mlody3k@gmail.com>
2026-06-25 01:18:12 +0200

    jobset simple

--- test/kubernetes/test_simple_jobset.py
+++ test/kubernetes/test_simple_jobset.py
@@ -24,7 +24,7 @@ def wait_for_finalized_jobset(core_v1, jobset_name, size=4, timeout=120):
     pytest.fail(f"JobSet {jobset_name} did not complete within {timeout}s")
 
 @pytest.fixture(scope="module")
-def job_logs(k8s_client, core_v1):
+def jobset_logs(k8s_client, core_v1):
     manifest_path = "manifests/jobset.yaml"
     with open(manifest_path) as f:
         manifest = yaml.safe_load(f)
@@ -63,6 +63,37 @@ def job_logs(k8s_client, core_v1):
         body=client.V1DeleteOptions(propagation_policy="Foreground")
     )
 
-def test_just_print(job_logs):
-    print(job_logs)
-    pass
+def test_all_pods_finished(jobset_logs):
+    assert len(jobset_logs) == 2
+
+def test_each_pod_knows_its_rank(jobset_logs):
+    for log in jobset_logs:
+        assert log.get("myrank") is not None, f"Missing 'myrank' in: {log}"
+
+def test_each_pod_knows_its_hostname(jobset_logs):
+    for log in jobset_logs:
+        assert log.get("myhostname") is not None, f"Missing 'myhostname' in: {log}"
+
+def test_each_pod_is_mutated_with_pmix_env_vars(jobset_logs):
+    for log in jobset_logs:
+        env = log.get("myenvs", {})
+        assert env.get("PMIX_RANK") is not None, f"Pod was not mutated! Missing PMIX_RANK in: {log}"
+        assert env.get("PMIX_NAMESPACE") is not None, f"Pod was not mutated! Missing PMIX_NAMESPACE in: {log}"
+
+def test_there_is_just_one_rank_zero(jobset_logs):
+    rank_0_logs = [log for log in jobset_logs if log.get("myrank") == 0]
+    assert len(rank_0_logs) == 1, f"Expected exactly one pod with rank 0, found {len(rank_0_logs)}"
+
+def test_each_pod_knows_sees_correct_world_size(jobset_logs):
+    for log in jobset_logs:
+        assert len(log.get("ranks", [])) == 2, f"Pod just sees itself. Expected 'ranks' length 4 in: {log}"
+
+def test_rank_0_has_rank_specific_data(jobset_logs):
+    # Find the log for rank 0
+    rank_0_log = next((log for log in jobset_logs if log.get("myrank") == 0), None)
+    assert rank_0_log is not None, "No log found for rank 0"
+    assert rank_0_log.get("myenvs") is not None, f"Rank 0 log does not contain 'myenvs': {rank_0_log}"
+
+    assert rank_0_log["myenvs"].get("RANK_SPECIFIC_DATA") is not None, f"Rank 0 log does not contain expected 'RANK_SPECIFIC_DATA': {rank_0_log}"
+    assert rank_0_log["myenvs"]["RANK_SPECIFIC_DATA"] == "I am rank 0, yo maan", f"Rank 0 log does not contain expected 'RANK_SPECIFIC_DATA' value: {rank_0_log}"
+
